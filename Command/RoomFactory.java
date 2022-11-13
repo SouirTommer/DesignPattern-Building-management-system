4450872e6b3a548cb10fd033ba02118c5451c551
Tommer <q622826@gmail.com>
2022-11-13 12:13:06 +0800

    fix some bug

--- Command/RoomFactory.java
+++ Command/RoomFactory.java
@@ -7,7 +7,6 @@ public class RoomFactory implements CommandFactory {
     Stack commands;
     Scanner sc;
     HashMap<Integer, Building> buildMap;
-    
 
     public RoomFactory(Scanner sc, HashMap<Integer, Building> buildMap, Stack commands) {
         this.sc = sc;
@@ -26,20 +25,25 @@ public class RoomFactory implements CommandFactory {
         System.out.println("Please enter command: [a|d|m]");
         System.out.println("a = add room, d = delete room, m = modify room");
         String Userinput = sc.nextLine();
+        sc.nextLine();
 
-
-        
         Command c = null;
-        if (Userinput.equals("a")){
-            c = new AddroomsCommand(sc, buildMap, buildingNo);
+
+        switch(Userinput){
+            case "a":
+                c = new AddroomsCommand(sc, buildMap, buildingNo);
+                break;
+            case "d":
+                c = new DeleteroomsCommand(sc, buildMap, buildingNo);
+                break;
+            case "c":
+                c = new ModifyroomsCommand(sc, buildMap, buildingNo);
+                break;
+            default:
+                System.out.println("Error");
+                break;
         }
-        if (Userinput.equals("d")){
-            c = new DeleteroomsCommand(sc, buildMap, buildingNo);
-        }
-        if (Userinput.equals("m")){
-            c = new ModifyroomsCommand(sc, buildMap, buildingNo);
-        }
+
         commands.push(c);
         return c;
-    }
-}
+    }}
--- main.java
+++ main.java
@@ -28,7 +28,7 @@ public class main {
         f.put("a", new CreateBuildingCommandFactory(sc,buildMap,commands));
         f.put("d", new DisplayCommandFactory(sc,buildMap));
         f.put("m", new ModifyBuildingCommandFactory(sc,buildMap,commands));
-        // f.put("e", new RoomFactory(sc,buildMap,commands));
+        f.put("e", new RoomFactory(sc,buildMap,commands));
         f.put("u", new UndoCommandFactory(commands,redos));
         f.put("r", new RedoCommandFactory(commands,redos));
         f.put("l", new ListUndoRedoCommandFactory(commands,redos));
@@ -42,17 +42,17 @@ public class main {
                     "a = add building, d = display buildings, m = modify building, e = edit rooms, u = undo, r = redo, l = list undo/redo, x = exit system");
             Userinput = sc.nextLine();
 
-            switch (Userinput) {
+            // switch (Userinput) {
 
-                // case "e": // Edit Rooms Command
-                //     command = new EditroomsCommand(sc, buildMap);
-                //     command.execute();
-                //     break;
+            //     // case "e": // Edit Rooms Command
+            //     //     command = new EditroomsCommand(sc, buildMap);
+            //     //     command.execute();
+            //     //     break;
 
-                default:
-                    // System.out.println("input error");
-                    break;
-            }
+            //     default:
+            //         // System.out.println("input error");
+            //         break;
+            // }
             command = f.get(Userinput).createCommand();
             command.execute();
         }
