fbc3b804e5ef8269d4d3c64f066bfa6dec9030bb
Tommer <q622826@gmail.com>
2022-12-18 16:05:30 +0800

    modify some comment

--- Command/ModifyroomsCommand.java
+++ Command/ModifyroomsCommand.java
@@ -1,5 +1,4 @@
 /*
-Student:   Ching Chun Hung 210020835 2B
 Last Edit  15/11/2022
 */
 package Command;
--- Memento/Memento.java
+++ Memento/Memento.java
@@ -1,5 +1,4 @@
 /*
-Student:   Ching Chun Hung 210020835 2B
 Last Edit  13/11/2022
 */
 package Memento;
